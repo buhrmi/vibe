4711578dd08b0335e726601eff12bc97b9a04f95
Stefan Buhrmester <buhrmi@gmail.com>
2022-03-20 23:16:18 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -30,7 +30,7 @@ export default ({mode}) => {
       Unocss({
         extractors: [extractorSvelte],
         presets: [
-          presetAttributify(),
+          //presetAttributify(),
           presetUno(),
         ]
       }),
