749b497e076f003f281b894364799fbfc16df0c5
Stefan Buhrmester <buhrmi@gmail.com>
2022-03-20 23:34:25 +0100

    text

--- app/frontend/pages/static/about.svelte
+++ app/frontend/pages/static/about.svelte
@@ -1,10 +1,12 @@
 <div class="container">
     <h1>About Vibe Tokyo</h1>
+    <p>
+        Vibe Tokyo is an in-development <a href="https://github.com/buhrmi/vibe">open source platform</a> to drive a new type of nightlife.
+    </p>
     <h2>Founders</h2>
     <p>
-        Vibe Tokyo was founded by Jeseph "<a target="_blank" href="https://www.instagram.com/twztdmuzik">TWZTD</a>" Davis
-        and Stefan "<a target="_blank" href="https://github.com/buhrmi">buhrmi</a>" Buhrmester. We're just getting started. In the meantime, follow our socials below
-        for the latest news and a good time.
+        Vibe Tokyo is founded by Jeseph "<a target="_blank" href="https://www.instagram.com/twztdmuzik">TWZTD</a>" Davis
+        and Stefan "<a target="_blank" href="https://github.com/buhrmi">buhrmi</a>" Buhrmester. We're just getting started. In the meantime, make sure to follow our socials below for the latest news.
     </p>
     <h2>Socials</h2>
     <a target="_blank" href="https://www.instagram.com/vibe_tokyo">@vibe_tokyo</a>
--- app/frontend/pages/static/invest.svelte
+++ app/frontend/pages/static/invest.svelte
@@ -6,16 +6,15 @@
 <main class="container">
   <h1>Are you ready to VIBE?</h1>
   <p>
-    We now accept Bitcoin to invest in VIBE. Send Bitcoin only from an address to which you hold the private keys. Do not send Bitcoin from an exchange.
+    You can use Bitcoin to invest in VIBE and become a part of our community. If you'd like to invest, make sure to send Bitcoin only from an address to which you hold the private keys, that is, do <strong>not</strong> send Bitcoin from an exchange.
   </p>
   {#if clicked}
-    <p>Thank you. Please send Bitcoin to the following address:</p>
+    <p>Thank you. To invest, please make a deposit to the following address:</p>
     <p>
     <a class="text-xl bg-red/20 p-3 inline-block" href="bitcoin:bc1qccdh533ecmcfzk5uu2s2qy36lvv2s22n07qxlx">bc1qccdh533ecmcfzk5uu2s2qy36lvv2s22n07qxlx</a>
-    
       <a target="_blank" href="https://www.blockchain.com/btc/address/bc1qccdh533ecmcfzk5uu2s2qy36lvv2s22n07qxlx">Check transactions on the Blockchain</a>
     </p>
-    <p>Afterwards, please join our Discord server for further instructions and announcements</p>
+    <p>Afterwards, please join our Discord server for further instructions and announcements.</p>
   {:else}
     <p>Please click the button below to reveal the address.</p>
     <p>
