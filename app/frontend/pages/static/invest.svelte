fb6b51665157e0be96fe9d415c5957d42894c505
Stefan Buhrmester <buhrmi@gmail.com>
2022-03-22 09:46:27 +0100

    Update invest.svelte

--- app/frontend/pages/static/invest.svelte
+++ app/frontend/pages/static/invest.svelte
@@ -6,7 +6,7 @@
 <main class="container">
   <h1>Are you ready to VIBE?</h1>
   <p>
-    You can use Bitcoin to invest in VIBE and become a part of our community. If you'd like to invest, make sure to send Bitcoin only from an address to which you hold the private keys, that is, do <strong>not</strong> send Bitcoin from an exchange.
+    You can use Bitcoin to invest in VIBE and become a part of our community. Make sure to send Bitcoin only from an address to which you hold the private keys, that is, do <strong>not</strong> send Bitcoin from an exchange.
   </p>
   {#if clicked}
     <p>Thank you. To invest, please make a deposit to the following address:</p>
@@ -14,7 +14,7 @@
     <a class="text-xl bg-red/20 p-3 inline-block" href="bitcoin:bc1qccdh533ecmcfzk5uu2s2qy36lvv2s22n07qxlx">bc1qccdh533ecmcfzk5uu2s2qy36lvv2s22n07qxlx</a>
       <a target="_blank" href="https://www.blockchain.com/btc/address/bc1qccdh533ecmcfzk5uu2s2qy36lvv2s22n07qxlx">Check transactions on the Blockchain</a>
     </p>
-    <p>Afterwards, please join our Discord server for further instructions and announcements.</p>
+    <p>Afterwards, please join our <a href="https://discord.gg/nrA8yDzSvf" target="_blank">Discord server</a> for further instructions and announcements.</p>
   {:else}
     <p>Please click the button below to reveal the address.</p>
     <p>
